9ca4430ffac73e949a45eb8a9d7d93f4921c5ee9
Y Vidhya Sagar Reddy <yvidhya.reddy@feisystems.com>
2024-11-11 16:05:43 +0530

    Commit 0

---
+++ components/src/lib/sidebar/sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-sidebar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css',
+})
+export class SidebarComponent {}
